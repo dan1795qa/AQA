1ec3abf63820d2e9b18c5342d88d95669fb967fb
Daniil <dan1795dan@gmail.com>
2025-08-21 22:59:55 +0300

    Add method GET in test

--- Pytest/Pytest-2/tests/test_google_maps_api.py
+++ Pytest/Pytest-2/tests/test_google_maps_api.py
@@ -8,3 +8,8 @@ class Test_create_place():
 
         print("Метод POST")
         result_post = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+
+        print("Метод GET")
+        result_get =  Google_maps_api.get_new_place(place_id)

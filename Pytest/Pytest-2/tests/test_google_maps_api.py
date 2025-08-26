32b0d073948013c3f55984f5b555f71d0dae8bdb
Daniil <dan1795dan@gmail.com>
2025-08-26 22:45:40 +0300

    Add method from Checking.py in tests

--- Pytest/Pytest-2/tests/test_google_maps_api.py
+++ Pytest/Pytest-2/tests/test_google_maps_api.py
@@ -1,5 +1,6 @@
 from requests import Response
 from utils.api import Google_maps_api
+from utils.checking import Checking
 
 """"Создание, изменение, удаление новой локации"""
 class Test_create_place():
@@ -10,18 +11,24 @@ class Test_create_place():
         result_post = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 300)
 
         print("Метод GET POST")
         result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
         print("Метод PUT")
         result_put = Google_maps_api.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
 
         print("Метод GET PUT")
         result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
         print("Метод DELETE")
         result_delete = Google_maps_api.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
 
         print("Метод GET DELETE")
         result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)

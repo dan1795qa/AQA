07d8756fe7b0dcd7fd76bb27bef6ebcf34329ba6
Daniil <dan1795dan@gmail.com>
2025-09-02 21:34:43 +0300

    Method to check for mandatory fields in the request response

--- Pytest/Pytest-2/tests/test_google_maps_api.py
+++ Pytest/Pytest-2/tests/test_google_maps_api.py
@@ -1,34 +1,52 @@
+import json
+
 from requests import Response
 from utils.api import Google_maps_api
 from utils.checking import Checking
 
 """"Создание, изменение, удаление новой локации"""
+
+
 class Test_create_place():
 
-    def test_create_neew_place(self):
-
+    def test_create_new_place(self):
         print("Метод POST")
         result_post = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
-        Checking.check_status_code(result_post, 300)
+        Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         print("Метод GET POST")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language']
+                                  )
 
         print("Метод PUT")
         result_put = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print("Метод GET PUT")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language']
+                                  )
 
         print("Метод DELETE")
         result_delete = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print("Метод GET DELETE")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+
+
+        print("Тестирование создания, изменения, удаления новой локации прошло успешно!!!")
--- Pytest/Pytest-2/utils/checking.py
+++ Pytest/Pytest-2/utils/checking.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -9,3 +11,10 @@ class Checking():
         """Метод для проверки статус кода"""
         assert status_code == result.status_code, 'ОШИБКА, Статус-код не совпадает'
         print(f"Успешно! Статус код = {result.status_code}")
+
+    @staticmethod
+    def check_json_token(result, expected_value):
+        """"Метод для проверки наличия обязательных полей в ответе запроса"""
+        fields = json.loads(result.text)        # преобразует строку в формат json
+        assert list(fields) == expected_value, 'ОШИБКА, Список полей не совпадает'
+        print("Все поля присутствуют")

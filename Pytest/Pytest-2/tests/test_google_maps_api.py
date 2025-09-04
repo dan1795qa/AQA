c3e989e0c5778abf565ea509f565582ce79d9fec
Daniil <dan1795dan@gmail.com>
2025-09-04 20:36:54 +0300

    Method for checking the values of mandatory fields in the query response by searching on a specific word

--- Pytest/Pytest-2/tests/test_google_maps_api.py
+++ Pytest/Pytest-2/tests/test_google_maps_api.py
@@ -16,6 +16,7 @@ class Test_create_place():
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
         Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(result_post, 'status', 'OK')
 
         print("Метод GET POST")
         result_get = Google_maps_api.get_new_place(place_id)
@@ -24,11 +25,13 @@ class Test_create_place():
                                   ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
                                    'language']
                                   )
+        Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
 
         print("Метод PUT")
         result_put = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
         Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
 
         print("Метод GET PUT")
         result_get = Google_maps_api.get_new_place(place_id)
@@ -37,16 +40,20 @@ class Test_create_place():
                                   ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
                                    'language']
                                   )
+        Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
 
         print("Метод DELETE")
         result_delete = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
-        Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_token(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
 
         print("Метод GET DELETE")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
         Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_value(result_get, 'msg', "Get operation failed, looks like place_id  doesn't exists")
+        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
 
 
         print("Тестирование создания, изменения, удаления новой локации прошло успешно!!!")
--- Pytest/Pytest-2/utils/checking.py
+++ Pytest/Pytest-2/utils/checking.py
@@ -15,6 +15,22 @@ class Checking():
     @staticmethod
     def check_json_token(result, expected_value):
         """"Метод для проверки наличия обязательных полей в ответе запроса"""
-        fields = json.loads(result.text)        # преобразует строку в формат json
+        fields = json.loads(result.text)  # преобразует строку в формат json
         assert list(fields) == expected_value, 'ОШИБКА, Список полей не совпадает'
         print("Все поля присутствуют")
+
+    @staticmethod
+    def check_json_value(result, field_name, expected_value):
+        """"Метод для проверки значений обязательных полей в ответе запроса"""
+        check = result.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value
+        print(f"{field_name} верно !!!")
+
+    @staticmethod
+    def check_json_search_word_in_value(result, field_name, search_word):
+        """Метод для проверки значений обязательных полей в ответе запроса при помощи поиска по определенному слову"""
+        check = result.json()
+        check_info = check.get(field_name)
+        assert search_word in check_info
+        print(f"Слово {search_word} присутствует")
